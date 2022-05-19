128c7021caba92133893a71fb9dc122535c0bf7d
ly <1290109033@qq.com>
2022-05-19 14:39:33 +0800

    在ly分支中添加了功能

--- src/learngit.java
+++ src/learngit.java
@@ -2,5 +2,6 @@ public class learngit {
     public static void main(String[] args) {
         System.out.println("hello,git!");
         System.out.println("对git文件进行了一次修改");
+        System.out.println("这是在ly分支中添加的功能");
     }
 }
